6d9348575902adb963f11600f96ddd1d14fb69bb
ccerneka <ccerneka29@setoncatholic.org>
2026-09-23 18:20:11 +0000

    calculator

---
+++ def add(x,y):.py
@@ -0,0 +1,25 @@
+#function that adds two numbers
+def add(x, y):
+    print(x + y)
+
+#function that subtracts two numbers
+def subtract(x, y):
+    print(x - y)
+
+#function that multiplies two numbers
+def multiply(x, y):
+    print(x * y)
+
+#function that divides two numbers
+def divide(x, y):
+    print(x / y)
+
+
+
+x = int(input("enter first number: "))
+y = int(input("enter second number: "))
+
+add(x, y)
+subtract(x, y)
+multiply(x, y)
+divide(x, y)

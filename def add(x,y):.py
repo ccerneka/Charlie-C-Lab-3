449d4e3df693a58f43279cca252e0f61e3f39263
ccerneka <ccerneka29@setoncatholic.org>
2026-09-24 20:56:11 +0000

    completed calc

--- def add(x,y):.py
+++ def add(x,y):.py
@@ -19,7 +19,30 @@ def divide(x, y):
 x = int(input("enter first number: "))
 y = int(input("enter second number: "))
 
-add(x, y)
-subtract(x, y)
-multiply(x, y)
-divide(x, y)
+
+
+#####################################
+print("Welcome")
+print("what would you like to do")
+print("Tpye (a)dd  (s)ubtract  (m)ultiply  (d)ivide")
+user_choice = input(":")
+#print(user_choice)
+
+while(True):
+    if user_choice == 'a': 
+        add(x, y)
+#elif s
+    elif user_choice=='s': 
+        subtract(x, y)
+#elif m
+    elif user_choice=='m':
+        multiply(x, y)
+#elif d
+    elif user_choice=='d':
+        divide(x, y)
+#elif q
+#break
+#shutting down
+    elif user_choice=='q':
+        print("shutting down")
+    break
